b25d73615648766051246f57b972c0f3a0da7191
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2024-04-23 14:37:31 +0200

    implement obtain subtiles

--- sentle/sentle.py
+++ sentle/sentle.py
@@ -1,4 +1,9 @@
 import atenea
+import itertools
+import numpy as np
+from rasterio import warp, windows, transform
+from shapely.geometry import box
+from rasterio.crs import CRS
 import zarr
 import rasterio.warp
 from typing import Optional, Union
@@ -8,51 +13,65 @@ import geopandas as gpd
 import pkg_resources
 
 
-class SentinelSubtile:
-
-    def __init__(self, sentinel_tile_string: str, left: float, bottom: float,
-                 right: float, top: float):
-        self.sentinel_tile_string = sentinel_tile_string
-        self.left = left
-        self.bottom = bottom
-        self.right = right
-        self.top = top
-
-    def __str__(self):
-        return str((sentinel_tile_string, self.left, self.bottom, self.right,
-                    self.top))
-
-
-def obtain_subtiles(target_crs: str, left: float, bottom: float, right: float,
-                    top: float, subtile_width: int) -> list[SentinelSubtile]:
+def obtain_subtiles(target_crs: CRS, left: float, bottom: float, right: float,
+                    top: float, subtile_size: int):
     """Retrieves the sentinel subtiles that intersect the with the specified
     bounds. The bounds are interpreted based on the given target_crs.
     """
+
+    # TODO make it possible to not only use naive bounds but also MultiPolygons
+
     # 0 check if supplied sub_tile_width makes sense
+    assert (subtile_size
+            >= 16) and (subtile_size
+                        <= 10980), "subtile_size needs to within 16 and 10980"
+    assert (10980 %
+            subtile_size) == 0, "subtile_size needs to be a divisor of 10980"
 
     # 1 load sentinel grid
     s2grid = gpd.read_file(
-        pkg_resources.resource_filename(
-            __name__,
-            "data/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml"
-        ))
+        pkg_resources.resource_filename(__name__,
+                                        "data/sentinel2_grid_stripped.gpkg"))
 
     # 2 convert box to sentinel grid crs
-    transformed_bounds = rasterio.warp.transform_bounds(src_crs=target_crs,
-                                                        dst_crs=s2grid.crs,
-                                                        left,
-                                                        bottom,
-                                                        right,
-                                                        top)
+    transformed_bounds = box(
+        *rasterio.warp.transform_bounds(src_crs=target_crs,
+                                        dst_crs=s2grid.crs,
+                                        left=left,
+                                        bottom=bottom,
+                                        right=right,
+                                        top=top))
 
     # 3 extract overlapping sentinel tiles
-    s2tiles = s2grid[s2grid["geometry"].intersects(
-        transform_bounds)].index.tolist()
+    s2grid = s2grid[s2grid["geometry"].intersects(transformed_bounds)]
 
-    subtiles = []
-    # 4 for each sentinel tile determin sub-sentinel tiles
-    for tile in s2tiles:
-        pass
+    general_subtile_windows = [
+        windows.Window(col_off, row_off, subtile_size, subtile_size)
+        for col_off, row_off in itertools.product(
+            np.arange(0, 10980, subtile_size), np.arange(
+                0, 10980, subtile_size))
+    ]
+
+    # get polygon in respective row-column domain of sentinel tile assuming 10m
+    # resolution
+    s2grid["bounds_rowcol"] = s2grid["geometry"].apply(
+        lambda geom: windows.from_bounds(
+            *transformed_bounds.bounds,
+            transform.from_bounds(*geom.bounds, 10980, 10980)))
+
+    # iterate through subtiles of each sentinel tile and determine whether they
+    # overlap with the bounds
+    s2grid["intersecting_windows"] = s2grid["bounds_rowcol"].apply(
+        lambda win_bound: [
+            win_subtile for win_subtile in general_subtile_windows
+            if windows.intersect([win_bound, win_subtile])
+        ])
+
+    # each line contains one subtile of a sentinel that we want to download and
+    # process because it intersects the specified bounds to download
+    s2grid = s2grid.explode("intersecting_windows")
+
+    return s2grid
 
 
 def process_subtile(subtile: SentinelSubtile, datetime: DatetimeLike,
@@ -80,25 +99,22 @@ def process_subtile(subtile: SentinelSubtile, datetime: DatetimeLike,
     pass
 
 
-def process(target_crs: str,
-            zarr_path: str,
+def process(target_crs: CRS,
             bound_left: float,
             bound_bottom: float,
             bound_right: float,
             bound_top: float,
             datetime: DatetimeLike,
-            subtile_width: int,
-            num_cores: int,
-            kwargs_atenea: dict): 
-
+            subtile_size: int,
+            num_cores: int = 1,
+            kwargs_atenea: dict = dict(),
+            zarr_path: str = None):
     """
     Parameters
     ----------
 
-    target_crs: str
+    target_crs: CRS
         Specifies the target CRS that all data will be reprojected to.
-    zarr_path: str, optional
-        Path where zarr storage is supposed to be created.
     bound_left: float
         Left bound of area that is supposed to be covered. Unit is in `target_crs`.
     bound_bottom: float
@@ -109,12 +125,14 @@ def process(target_crs: str,
         Top bound of area that is supposed to be covered. Unit is in `target_crs`.
     datetime: DatetimeLike
         Specifies time range of data to be downloaded. This is forwarded to the respective stac interface.
-    subtile_width: int
-        TODO
-    num_cores: int
+    subtile_size: int, default=732
+        Specifies the size of each subtile. The maximum is the size of a sentinel tile (10980). If cloud filtering is enabled the minimum tilesize is 256, otherwise 16. It also needs to be a divisor of 10980, so that each sentinel tile can be segmented without overlaps.
+    num_cores: int, default = 1
         Number of CPU cores across which subtile processing is supposed to be distributed.
-    kwargs_atenea: dict, optional
+    kwargs_atenea: dict, default = None
         Arguments passed to atenea specifying processing steps that are applied to each tile.
+    zarr_path: str, default = None
+        Path where zarr storage is supposed to be created.
     """
 
     if zarr_path is None:
@@ -131,7 +149,9 @@ def process(target_crs: str,
                                bound_bottom,
                                bound_right,
                                bound_top,
-                               subtile_width=subtile_width)
+                               subtile_size=subtile_size)
+
+    return
 
     # create zarr storage
     # for each band and mask
@@ -162,3 +182,14 @@ def process(target_crs: str,
     # possible ISSUE: what happens when not everything fits into memory --> we
     # may want to have each subtile already being written to the harddrive once
     # it's done --> is that supported with ZARR?
+
+
+process(
+    CRS.from_string("EPSG:8857"),
+    bound_left=767300,
+    bound_bottom=7290000,
+    bound_right=776000,
+    bound_top=7315000,
+    datetime="2023-12-01/2023-11-01",
+    subtile_size=732,
+)
