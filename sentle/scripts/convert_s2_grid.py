6e4b3e08f32cda77a75bdba4dca49a5e12657c75
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2024-05-12 16:28:45 +0200

    add script to convert kml to gpkg for sentinel grid

---
+++ sentle/scripts/convert_s2_grid.py
@@ -0,0 +1,17 @@
+import geopandas as gpd
+import fiona
+import pandas as pd
+from io import StringIO
+fiona.drvsupport.supported_drivers['KML'] = "rw"
+
+df = gpd.read_file("../data/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml")
+
+df = df[["Name", "geometry", "Description"]]
+
+df["epsg_code"] = df["Description"].apply(lambda desc: pd.read_html(StringIO(desc))[0][1].iloc[1])
+
+df = df.rename(columns=dict(Name="name"))
+
+print(df)
+
+df[["name", "geometry", "epsg_code"]].to_file("../data/sentinel2_grid_stripped_with_epsg.gpkg")
