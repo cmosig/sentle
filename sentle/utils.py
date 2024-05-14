7161e9eeb06ea6e67317aea3e2267dfaeccaed65
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2024-05-14 17:29:32 +0200

    remove unused functions

--- sentle/utils.py
+++ sentle/utils.py
@@ -1,8 +1,3 @@
-from joblib import Parallel, delayed
-from typing import Tuple, Iterable, Set, List, Callable
-import joblib
-from tqdm import tqdm
-import contextlib
 import utm
 
 S2_RAW_BANDS = [
@@ -23,50 +18,6 @@ S2_RAW_BAND_RESOLUTION = {
     'B11': 20,
     'B12': 20
 }
-
-
-@contextlib.contextmanager
-def tqdm_joblib(tqdm_object):
-    """Context manager to patch joblib to report into tqdm progress bar given as argument"""
-
-    # credits:
-    # https://stackoverflow.com/questions/24983493/tracking-progress-of-joblib-parallel-execution
-    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-        def __call__(self, *args, **kwargs):
-            tqdm_object.update(n=self.batch_size)
-            return super().__call__(*args, **kwargs)
-
-    old_batch_callback = joblib.parallel.BatchCompletionCallBack
-    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
-    try:
-        yield tqdm_object
-    finally:
-        joblib.parallel.BatchCompletionCallBack = old_batch_callback
-        tqdm_object.close()
-
-
-def paral(function: Callable,
-          iters: List[Iterable],
-          num_cores=-1,
-          progress_bar=True,
-          backend="loky"):
-    """ compute function parallel with arguments in iters.
-    function(iters[0][0],iters[0][1],...)"""
-
-    with tqdm_joblib(
-            tqdm(desc=function.__name__,
-                 unit="jobs",
-                 dynamic_ncols=True,
-                 total=len(iters[0]),
-                 disable=not progress_bar), ) as progress_bar:
-        # backend can be loky or threading (or maybe something else)
-        return Parallel(n_jobs=num_cores, batch_size=1,
-                        backend=backend)(delayed(function)(*its)
-                                         for its in zip(*iters))
 
 
 def bounds_from_transform_height_width_res(transform, height, width,
