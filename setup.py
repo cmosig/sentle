ad27e20529bea40295b463ff10f0a3e165b5b711
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2024-11-12 18:09:04 +0100

    fix inclusion of cloudmodel #20

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
-    package_data={"sentle": ["./data/sentinel2_grid_stripped_with_epsg.gpkg"]},
+    package_data={"sentle": ["./data/sentinel2_grid_stripped_with_epsg.gpkg", "./data/cloudmodel.pt"]},
     install_requires=[
         "pystac-client>=0.7.7", "pystac>=1.10.1", "rasterio>=1.3.10",
         "affine>=2.4.0", "pandas>=2.2.2", "numpy>=1.26.4", "shapely>=2.0.4",
